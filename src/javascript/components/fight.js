d4192a6310207f8de62a6a8ca23005ef5e59f7b0
Dmitriy <dima.komlyk@gmail.com>
2022-06-07 15:52:08 +0300

    Created criticalHit function

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -3,7 +3,12 @@ import { controls } from '../../constants/controls';
 export async function fight(firstFighter, secondFighter) {
   return new Promise((resolve) => {
     // resolve the promise with the winner when fight is over
+
   });
+}
+export function criticalHit(fighter) {
+  const { attack } = fighter;
+  return attack * 2;
 }
 
 export function getDamage(attacker, defender) {
