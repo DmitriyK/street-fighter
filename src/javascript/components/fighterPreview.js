51725ecc29d481e1558d83632de51b076cbd7765
Dmitriy <dima.komlyk@gmail.com>
2022-06-07 13:30:53 +0300

    Created createFighterInfo function for info on fighter details. Updated createFighterPreview

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -6,10 +6,33 @@ export function createFighterPreview(fighter, position) {
     tagName: 'div',
     className: `fighter-preview___root ${positionClassName}`,
   });
-
   // todo: show fighter info (image, name, health, etc.)
+  if (fighter) {
+    const fighterImg = createFighterImage(fighter);
+    const fighterInfo = createFighterInfo(fighter);
 
+    fighterElement.append(fighterImg);
+    fighterElement.append(fighterInfo);
+  }
   return fighterElement;
+}
+
+function createFighterInfo(fighter) {
+  const { _id, source, ...infoData } = fighter;
+  const fighterInfoList = createElement({
+    tagName: 'ul',
+    className: 'fighter-preview___info',
+  })
+  for (const prop in infoData) {
+    const fighterInfoItem = createElement({
+      tagName: 'li',
+      className: 'fighter-preview___info-item',
+    })
+    fighterInfoItem.innerHTML = `<span>${prop}</span> - ${infoData[prop]}`;
+    fighterInfoList.append(fighterInfoItem);
+  }
+
+  return fighterInfoList;
 }
 
 export function createFighterImage(fighter) {
