d9b687f68500ae0ef9755564e3db060b8a4f7553
Dmitriy <dima.komlyk@gmail.com>
2022-06-08 13:02:09 +0300

    Updated showWinnerModal function. Created body and image element for showModal

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,3 +1,23 @@
+import { showModal } from './modal';
+import { createElement } from '../../helpers/domHelper';
+
 export function showWinnerModal(fighter) {
-  // call showModal function 
+  const { name, source } = fighter
+  const title = `${fighter.name} is winner`;
+
+  const bodyElement = createElement({
+    tagName: 'div',
+    className: 'modal-body'
+  })
+
+  const imageElement = createElement({
+    tagName: 'img',
+    className: 'fighter___winner',
+    attributes: { 
+      src: source,
+      title: name,
+      alt: name, },
+  })
+  bodyElement.append(imageElement)
+  showModal({ title, bodyElement });
 }
